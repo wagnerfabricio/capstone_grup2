837f35a7bcb71691f422bd8e37dec8039ee1ce32
Bruno Faria <soares_de_faria@hotmail.com>
2022-04-27 09:41:30 -0300

    feat(products/categories): Created Products and Categories models and POST and GET on controllers

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .hello_world_model import HelloWorld
 from .user_class_model import UserClassModel
 from .user_model import UserModel
+from .categories import Categories
+from .products_model import Products
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, Flask
 from .hello_world_blueprint import bp as bp_hello_world
 from .user_blueprint import bp as bp_user
-
+from .products_blueprint import bp as bp_products
+from .categories_blueprint import bp as bp_categories
 
 api = Blueprint('api', __name__)
 
 def init_app(app: Flask):
     api.register_blueprint(bp_hello_world)
     api.register_blueprint(bp_user)
+    api.register_blueprint(bp_products)
+    api.register_blueprint(bp_categories)
 
     app.register_blueprint(api)
